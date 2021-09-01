8a2acddd8e303222f6ecbb59b2b5fc8cc16653c4
Sam Potasznik <sampotasznik@gmail.com>
2021-09-01 14:08:32 -0500

    changed result highlighted word to link

--- highlight.py
+++ highlight.py
@@ -33,7 +33,7 @@ def highlight(text: str, terms_list: List[Tuple[int, int]]) -> str:
             # i.e. if the original text has \n, should use that instead of space
             result += f"{doc[previous_end:term_start].text} "
         # get all highlighted terms
-        result += f'<mark style="background: #FFBE2E!important; cursor: pointer; " onclick="myFunction(\'{word_id}\')">{word}</mark>'
+        result += f'<a class="usa-link" role="button" style="background: #FFBE2E!important; cursor: pointer; " onclick="myFunction(\'{word_id}\')">{word}</a>'
         # if the current highlighted term is in the middle of the doc, right pad with a space
         if term_end < len(doc):
             result += " "
